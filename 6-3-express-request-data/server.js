c230734bd3a3a7154e701e53f21c358082ff4a9f
KarrarAlqallaf <karrar3035@gmail.com>
2025-11-11 11:34:22 +0300

    todo 1 done

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -106,8 +106,8 @@ LAB SETUP INSTRUCTIONS
 
 import express from "express";
 const app = express();
-
-
+const port =  3000;
+app.listen(port, ()=> console.log(`API running at http://localhost:${port}`));
 // create server
 
 
